0fa01defb6542cd3714efb732f82ba36733705db
MC <chokamoses2@gmail.com>
2023-11-17 15:42:32 +0300

    Finish initial static pages

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,13 +1,25 @@
 require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
+ 
+  def setup
+    @base_title = "DataSessions, For Data Enthusiasts"
+  end
+
+  test "should get root" do
+    get pages_home_url
+    assert_response :success
+  end
+
   test "should get home" do
     get pages_home_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get about" do
     get pages_about_url
     assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
 end
