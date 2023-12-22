5d30de4afe6dc406f367b1342874c6fa8826c531
MC <chokamoses2@gmail.com>
2023-12-22 21:43:52 +0300

    Add user path, gravatar, and sidebar

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'pages/home'
   get "/about", to: "pages#about"
   get "/contact", to: "pages#contact"
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
