796ba058f1c8993d89acbec547a876bb9a6cd845
MC <chokamoses2@gmail.com>
2024-01-05 22:52:07 +0300

    Add a user login/logout controller session with form

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   get "/signup", to:"users#new"
   root 'pages#home'
   get 'pages/home'
-  get "/about", to: "pages#about"
-  get "/contact", to: "pages#contact"
+  get    "/about", to: "pages#about"
+  get    "/contact", to: "pages#contact"
+  get    "/login", to: "sessions#new"
+  post   "/login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
